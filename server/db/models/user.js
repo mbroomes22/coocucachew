d90d096d61587fc01d71db456b8bdecfb4acf78a
Adriana Winkelman <adriana.winkelman@gmail.com>
2020-04-22 16:44:44 -0400

    User model updated

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -16,6 +16,14 @@ const User = db.define('user', {
       return () => this.getDataValue('password')
     }
   },
+  isLoggedIn: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
