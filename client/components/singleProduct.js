acb417f179aadf5fdfaf5af785856739171e3c35
Adriana Winkelman <adriana.winkelman@gmail.com>
2020-04-23 16:08:30 -0400

    Changes made on singleComponent button quantity

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -46,12 +46,17 @@ export class SingleProduct extends Component {
           <div className="quantity_input">
             <form>
               <select id="product_quantity">
-                {/* <option selected="">Qty</option> */}
+                <option defaultValue="">Qty</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
               </select>
               <button type="submit">add to cart</button>
             </form>
