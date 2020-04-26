5e3b09b004620d11388200db173c339804dd6ed0
Adriana Winkelman <adriana.winkelman@gmail.com>
2020-04-26 13:44:35 -0400

    added subtotal on model Order

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
+  subTotal: {
+    // when and how will we calculate this?
+    type: Sequelize.INTEGER
+  },
   total: {
     // when and how will we calculate this?
     type: Sequelize.INTEGER
