794ec600c561d22f123562f882b0be3bc4891176
GiselleDZ <giselle@gisellezatonyl.com>
2020-04-26 15:01:45 -0400

    merged test1 with checkout branch, changed import file names in components index js, to test travis

--- client/components/index.js
+++ client/components/index.js
@@ -3,10 +3,10 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as AddProductForm} from './addProductForm'
+export {default as AddProductForm} from './addProductForm.jsx'
 export {default as AllProducts} from './allProducts'
-export {default as Cart} from './cart'
-export {default as CartList} from './cartList'
+export {default as Cart} from './cart.jsx'
+export {default as CartList} from './cartList.jsx'
 export {default as Navbar} from './navbar'
 export {default as SingleProduct} from './singleProduct'
 export {default as UserHome} from './userHome'
