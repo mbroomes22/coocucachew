350cb14711c7bcb7eefbd09ac12287c5225d3964
GiselleDZ <giselle@gisellezatonyl.com>
2020-04-27 05:25:49 -0400

    overhauled cart structure, unfinished step process

--- client/components/cart/cartMain.jsx
+++ client/components/cart/cartMain.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchCart} from '../store/cart'
+import {fetchCart} from '../../store/cartStore'
 import {CartList} from './cartList'
-// import {userForm} from './Checkout/userForm'
 
-const defaultCart = {
-  id: 0,
-  isPending: false,
-  products: []
-}
-export class Cart extends React.Component {
+export class CartMain extends React.Component {
   async componentDidMount() {
     await this.props.getCart()
   }
@@ -19,7 +13,6 @@ export class Cart extends React.Component {
       <div>
         <h3>C A R T</h3>
         <CartList cart={this.props.cart} />
-        {/* <userForm cart={this.props.cart} /> */}
       </div>
     )
   }
@@ -33,4 +26,4 @@ const mapDispatch = dispatch => ({
   getCart: () => dispatch(fetchCart())
 })
 
-export default connect(mapState, mapDispatch)(Cart)
+export default connect(mapState, mapDispatch)(CartMain)
--- client/components/index.js
+++ client/components/index.js
@@ -3,11 +3,15 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as AddProductForm} from './AddProductForm'
-export {default as AllProducts} from './AllProducts'
-export {default as Cart} from './cart'
-export {default as CartList} from './cartList'
+export {default as AddProductForm} from './addProductForm'
+export {default as AllProducts} from './allProducts'
+export {default as CartMain} from './cart/cartMain'
+export {default as CartList} from './cart/cartList'
+export {default as CartProducts} from './cart/cartProducts'
+export {default as CartQuantity} from './cart/cartQuantity'
+
 export {default as Navbar} from './navbar'
 export {default as SingleProduct} from './singleProduct'
 export {default as UserHome} from './userHome'
+export {default as UserForm} from './checkout/userForm'
 export {Login, Signup} from './authForm'
